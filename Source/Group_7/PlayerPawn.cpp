27f081d21e7a13ee06c04851091a0c3efdb89cef
S4r43l <selauvstad@gmail.com>
2023-03-09 15:59:16 +0100

    PlayerPawn StartUp
    - Started writing the PlayerPawn

--- Source/Group_7/PlayerPawn.cpp
+++ Source/Group_7/PlayerPawn.cpp
@@ -2,12 +2,46 @@
 
 
 #include "PlayerPawn.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputTriggers.h"
 
 // Sets default values
 APlayerPawn::APlayerPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SetRootComponent(StaticMesh);
+	StaticMesh->SetRelativeLocation(FVector(0, 0, 0));
+	StaticMesh->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f));
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 15.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	// Stand still when we dont move
+	bUseControllerRotationYaw = false;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+
+	AmmoCount = 10;
+	MaxAmmo = 10;
+	GranadeCount = 10;
+	MaxGranade = 10;
+	MovementSpeed = 1000;
+	Lives = 5;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 }
 
@@ -32,3 +66,30 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 
 }
 
+//void APlayerPawn::ForwardBackward(const FInputActionValue& input)
+//{
+//}
+//
+//void APlayerPawn::RightLeft(const FInputActionValue& input)
+//{
+//}
+//
+//void APlayerPawn::Shoot(const FInputActionValue& input)
+//{
+//}
+//
+//void APlayerPawn::Reload(const FInputActionValue& input)
+//{
+//}
+//
+//void APlayerPawn::Throw(const FInputActionValue& input)
+//{
+//}
+//
+//void APlayerPawn::MouseX(const FInputActionValue& input)
+//{
+//}
+//
+//void APlayerPawn::MouseY(const FInputActionValue& input)
+//{
+//}
--- Source/Group_7/PlayerPawn.h
+++ Source/Group_7/PlayerPawn.h
@@ -23,6 +23,75 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+			/** Components */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UCameraComponent* Camera;
+
+	/** Public Variables */
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int AmmoCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int MaxAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int GranadeCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int MaxGranade;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int Lives;
+
+			/** Input */ 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputMappingContext* IMCinput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* ForwardBackwardInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* RightLeftInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* ShootInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* ReloadInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* ThrowInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* MouseXInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = InputSystem)
+		class UInputAction* MouseYInput;
+
+private:
+	/** Private Functions */
+
+	//void ForwardBackward(const FInputActionValue& input);
+	//void RightLeft(const FInputActionValue& input);
+
+	//void Shoot(const FInputActionValue& input);
+	//void Reload(const FInputActionValue& input);
+	//void Throw(const FInputActionValue& input);
+
+	//void MouseX(const FInputActionValue& input);
+	//void MouseY(const FInputActionValue& input);
+
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
