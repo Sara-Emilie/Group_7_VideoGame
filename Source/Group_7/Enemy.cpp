d4570f4d3fa0470b9c2dbbb690fbaa0aff30be33
tordelias <tordeliashoumb46@gmail.com>
2023-03-22 14:53:24 +0100

    Added som stuff on enemy

--- Source/Group_7/Enemy.cpp
+++ Source/Group_7/Enemy.cpp
@@ -2,12 +2,30 @@
 
 
 #include "Enemy.h"
+#include "Kismet/GameplayStatics.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Bullet.h"
+#include "InputTriggers.h"
+
+class ABullet; 
 
 // Sets default values
 AEnemy::AEnemy()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationYaw = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMesh->SetupAttachment(GetRootComponent()); //Root is Character's capsule
+	StaticMesh->SetRelativeLocation(FVector(0, 0, 0));
+	StaticMesh->SetRelativeScale3D(FVector(0.5f, 0.5f, 0.5f));
+
+	bUseControllerRotationYaw = false;
+
+	MovementSpeed = 1000;
+	EnemyHealth = 2;
 
 }
 
@@ -31,4 +49,30 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
+void AEnemy::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->IsA<ABullet>()) {
+		Cast<ABullet>(OtherActor)->DestroyBullet();
+		
+		EnemyHealth--;
+
+		if (EnemyHealth <= 0)
+		{
+			DestoryTarget();
+		}
+	}
+}
+
+void AEnemy::DestoryTarget()
+{
+	SetActorHiddenInGame(true);
+	SetActorEnableCollision(false);
+
+	//AEnemySpawner* Spawner;
+
+	this->Destroy();
+}
+
+
 
--- Source/Group_7/Enemy.h
+++ Source/Group_7/Enemy.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "Enemy.generated.h"
+
+class UEnhancedInputComponent;
 
 UCLASS()
 class GROUP_7_API AEnemy : public ACharacter
@@ -25,5 +28,24 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMeshComponent* StaticMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		float MovementSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "My Variables")
+		int EnemyHealth;
+
+	UFUNCTION()
+		void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+			UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex,
+			bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void DestoryTarget();
+
+private:
+	/** Private Functions */
+
+
 
 };
